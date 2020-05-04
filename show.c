dede050c3f6d5f7921a90d409bffae10600d8d21
Lim yeonkyu <21900613@handong.edu>
2020-05-04 15:31:21 +0900

    create searchQuantity

--- show.c
+++ show.c
@@ -39,11 +39,29 @@ void searchName(Product *p,int count){
 void searchPrice(Product *p,int count){
 	int price=0,scoutn=0;
 	printf("검색할 가격?(이하로 검색)");
-
+	scanf("%d",&price);
 	printf("\nNo\tname\tcategory\tprice\tquantity");
 	for(int i=0;i<count;i++){
 		if(p[i].price !=-1){
 			if(price>=p[i].price){
+			printf("%2d",i+1);
+			readProduct(p[i]);
+			scount++;
+			}
+		}
+	}
+
+	if(scount==0)
+		printf("=> 검색된 데이터 없음!");
+}
+void searchQuantity(Product *p,int count){
+	int quantity=0,scoutn=0;
+	printf("검색할 가격?(이상으로 검색)");
+	scanf("%d",&quantity);
+	printf("\nNo\tname\tcategory\tprice\tquantity");
+	for(int i=0;i<count;i++){
+		if(p[i].price !=-1){
+			if(quantity<=p[i].quantity){
 			printf("%2d",i+1);
 			readProduct(p[i]);
 			scount++;
