f580f5b1d0c2a249929126d15191fab9bda95565
Hwang suhwan <hsh010126@gmail.com>
2020-05-09 16:29:33 +0900

    Make Debug code

--- Makefile
+++ Makefile
@@ -1,8 +1,13 @@
 CC=gcc
 TP=manage
+DP=Dmanage
 DEPENDENCY=main.c show.o manage.o
+
+$(DP):$(DEPENDENCY)
+	$(CC) -DDEBUG -o $@ $^
 
 $(TP):$(DEPENDENCY)
 	$(CC) -o $@ $^
+
 clean:
-	rm *.o manage
+	rm *.o manage Dmanage
--- main.c
+++ main.c
@@ -7,32 +7,65 @@ int mn;//menu
 int count=0;
 int save=0;
 int money=0;
+	#ifdef DEBUG
+		printf("DEBUG[loadData]\n");
+	#endif	
 	count=loadData(p);
 	while(1){
 		menu();
 		scanf("%d",&mn);
 	switch(mn){
 		case 1:
+	#ifdef DEBUG
+		printf("DEBUG[addProduct]\n");
+	#endif	
 			addProduct(p,count);
 			count++;
 			break;
 		case 2:
+	#ifdef DEBUG
+		printf("DEBUG[listProduct]\n");
+	#endif	
 			listProduct(p,count);
+	#ifdef DEBUG
+		printf("DEBUG[updateProduct]\n");
+	#endif	
 			updateProduct(p,count);
 			break;
 		case 3:
+	#ifdef DEBUG
+		printf("DEBUG[listProduct]\n");
+	#endif	
 			listProduct(p,count);
+	#ifdef DEBUG
+		printf("DEBUG[deleteProduct]\n");
+	#endif	
 			deleteProduct(p,count);
 			break;
 		case 4:
+	#ifdef DEBUG
+		printf("DEBUG[listProduct]\n");
+	#endif	
 			listProduct(p,count);
+	#ifdef DEBUG
+		printf("DEBUG[updateQuantity]\n");
+	#endif	
 			updateQuantity(p,count);
 			break;
 		case 5:
+	#ifdef DEBUG
+		printf("DEBUG[listProduct]\n");
+	#endif	
 			listProduct(p,count);
+	#ifdef DEBUG
+		printf("DEBUG[sellProduct]\n");
+	#endif	
 			sellProduct(p,count);
 			break;
 		case 6:
+	#ifdef DEBUG
+		printf("DEBUG[listProduct]\n");
+	#endif	
 			listProduct(p,count);
 			break;
 		case 7:
@@ -42,10 +75,16 @@ int money=0;
 			//searchProduct
 			break;
 		case 9:
+	#ifdef DEBUG
+		printf("DEBUG[saveData]\n");
+	#endif	
 			saveData(p,count);
 			save=1;
 			break;
 		case 10:
+	#ifdef DEBUG
+		printf("DEBUG[loadData]\n");
+	#endif	
 			count=loadData(p);
 			break;
 		case 0:
@@ -61,9 +100,12 @@ int money=0;
 		 printf("변동 사항이 저장되지 않았습니다.\n 그냥 종료하시겠 습니까?\n(0.저장후 종료한다. 1.저장하지 않고  종료한다.)\n");
 		scanf("%d",&save);
 		if(save==0){
+	#ifdef DEBUG
+		printf("DEBUG[saveData]\n");
+	#endif	
 			saveData(p,count);
 			save=1;
 		}
 	}
 	printf("프로그램을 종료합니다.\n");
-}	
+}
